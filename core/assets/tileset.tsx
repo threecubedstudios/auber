a13811f87f1505fc138199d1c7d8997c3e598689
Daniel <dob9601@gmail.com>
2020-11-15 03:11:33 +0000

    Tileset animations removed

--- core/assets/tileset.tsx
+++ core/assets/tileset.tsx
@@ -1,40 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="tileset" tilewidth="16" tileheight="16" tilecount="144" columns="12">
  <image source="tileset.png" width="192" height="192"/>
- <tile id="96">
-  <animation>
-   <frame tileid="96" duration="20"/>
-   <frame tileid="98" duration="20"/>
-   <frame tileid="100" duration="20"/>
-   <frame tileid="102" duration="20"/>
-   <frame tileid="108" duration="20"/>
-   <frame tileid="110" duration="20"/>
-   <frame tileid="112" duration="20"/>
-   <frame tileid="114" duration="20"/>
-   <frame tileid="120" duration="20"/>
-   <frame tileid="122" duration="20"/>
-   <frame tileid="124" duration="20"/>
-   <frame tileid="132" duration="20"/>
-   <frame tileid="134" duration="20"/>
-   <frame tileid="136" duration="20"/>
-  </animation>
- </tile>
- <tile id="97">
-  <animation>
-   <frame tileid="97" duration="20"/>
-   <frame tileid="99" duration="20"/>
-   <frame tileid="101" duration="20"/>
-   <frame tileid="103" duration="20"/>
-   <frame tileid="109" duration="20"/>
-   <frame tileid="111" duration="20"/>
-   <frame tileid="113" duration="20"/>
-   <frame tileid="115" duration="20"/>
-   <frame tileid="121" duration="20"/>
-   <frame tileid="123" duration="20"/>
-   <frame tileid="125" duration="20"/>
-   <frame tileid="133" duration="20"/>
-   <frame tileid="135" duration="20"/>
-   <frame tileid="137" duration="20"/>
-  </animation>
- </tile>
 </tileset>

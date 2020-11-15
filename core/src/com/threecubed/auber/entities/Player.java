c519df7ab021c6ac8cb7f68de511e4c0f3d613d6
Daniel <dob9601@gmail.com>
2020-11-15 03:12:43 +0000

    Removal of unneeded import

--- core/src/com/threecubed/auber/entities/Player.java
+++ core/src/com/threecubed/auber/entities/Player.java
@@ -1,7 +1,5 @@
 package com.threecubed.auber.entities;
 
-import java.util.List;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Camera;
@@ -14,12 +12,13 @@ import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Intersector;
-import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.Timer.Task;
 import com.threecubed.auber.Utils;
+import java.util.List;
+
 
 public class Player extends GameEntity {
   private static Texture texture = new Texture("player.png");  

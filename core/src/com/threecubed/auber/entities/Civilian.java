eb1756a0d6152d5f4de424af4752c775a4ac57f3
Daniel <dob9601@gmail.com>
2020-11-17 01:07:32 +0000

    Useless methods removed from civilian.

--- core/src/com/threecubed/auber/entities/Civilian.java
+++ core/src/com/threecubed/auber/entities/Civilian.java
@@ -1,28 +1,14 @@
 package com.threecubed.auber.entities;
 
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.threecubed.auber.World;
 
 
 public class Civilian extends Npc {
-  private static Texture texture = new Texture("player.png");  
-  
+  private static Texture texture = new Texture("civilian.png");
+
   public Civilian(float x, float y, TiledMap map) {
     super(x, y, texture, map);
-    
-  }
-
-  @Override
-  public void update(World world) {
-    super.update(world);
-  }
-
-  @Override
-  public void render(Batch batch, Camera camera) {
-    super.render(batch, camera);
   }
 }
 

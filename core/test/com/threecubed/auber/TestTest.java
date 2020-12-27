9c063465a97b219178c5bedc1aa267d038a560e5
Marks Polakovs <marks@markspolakovs.me>
2020-12-27 16:14:31 +0000

    Add a test test

---
+++ core/test/com/threecubed/auber/TestTest.java
@@ -0,0 +1,11 @@
+package com.threecubed.auber;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestTest {
+    @Test
+    public void testyTestTest() {
+        assertEquals(1, 1);
+    }
+}

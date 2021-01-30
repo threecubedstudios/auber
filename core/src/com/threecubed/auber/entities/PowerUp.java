9f636a8c5ef84c90c295c2716f1b4b7bf8c34ec0
Harry Kelly <73826459+hk944@users.noreply.github.com>
2021-01-30 16:35:13 +0000

    Added javadoc for power up

--- core/src/com/threecubed/auber/entities/PowerUp.java
+++ core/src/com/threecubed/auber/entities/PowerUp.java
@@ -2,6 +2,14 @@ package com.threecubed.auber.entities;
 
 import com.threecubed.auber.World;
 
+/**
+ * The power up entity that the player may pick up. Handles searching for player and calling methods for relevant
+ * power up.
+ *
+ * @author Harry Kelly
+ * @version 1.0
+ * @since 1.0
+ * */
 public class PowerUp extends GameEntity{
 
     public PowerUp(float x, float y, World world) {

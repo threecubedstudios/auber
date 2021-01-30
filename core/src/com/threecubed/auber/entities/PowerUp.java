4f904b2b4a807fbecc37cecc193221a9a2b278d6
Harry Kelly <73826459+hk944@users.noreply.github.com>
2021-01-30 16:36:59 +0000

    Implemented player detection

--- core/src/com/threecubed/auber/entities/PowerUp.java
+++ core/src/com/threecubed/auber/entities/PowerUp.java
@@ -12,12 +12,23 @@ import com.threecubed.auber.World;
  * */
 public class PowerUp extends GameEntity{
 
+    private float playerDetectRadius = 10f;
     public PowerUp(float x, float y, World world) {
         super(x, y, world.atlas.createSprite("projectile"));
+    }
+
+    private float getPlayerDistance(World world){
+        Vector2 playerPos = world.player.position;
+        Vector2 thisPos = this.position;
+        float deltaX = playerPos.x - thisPos.x;
+        float deltaY = playerPos.y - thisPos.y;
+        return (float) Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
     }
 
     @Override
     public void update(World world) {
 
+        if (getPlayerDistance(world) < playerDetectRadius){
+        }
     }
 }

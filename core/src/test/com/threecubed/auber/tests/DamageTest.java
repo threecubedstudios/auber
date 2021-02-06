21c41a885fec8c82c553495c810598b7e6346ad2
Lewis-McKenzie <53063230+Lewis-McKenzie@users.noreply.github.com>
2021-02-06 14:49:27 +0000

    Difficulty Test

--- core/src/test/com/threecubed/auber/tests/DamageTest.java
+++ core/src/test/com/threecubed/auber/tests/DamageTest.java
@@ -48,4 +48,23 @@ public class DamageTest {
     assertEquals("player's health is : " + worldMock.player.health,
             0.8f, worldMock.player.health, 0.0);
   }
+
+  @Test
+  public void minHealth() throws Exception{
+    assertNotNull(worldMock);
+    playerMock.immune = false;    
+    playerMock.health = 0f;
+    playerMock.playerTimer = new Timer();
+    playerMock.position = new Vector2(0,0);
+    worldMock.atlas = new TextureAtlas("auber.atlas");
+    playerMock.sprite = worldMock.atlas.createSprite("player"); 
+    worldMock.player = playerMock;
+
+    Difficulty.damageMultiplier = 1;
+
+    Projectile p = new Projectile(0, 0, new Vector2(0,0), infiltratorMock, CollisionActions.BLIND, worldMock);  
+    p.update(worldMock);
+    assertEquals("player's health is : " + worldMock.player.health,
+            -0.2f, worldMock.player.health, 0.0);
+  }
 }
---
+++ core/src/test/com/threecubed/auber/tests/DifficultyTest.java
@@ -0,0 +1,40 @@
+package com.threecubed.auber.tests;
+
+import static org.junit.Assert.*;
+import com.threecubed.auber.Difficulty;
+import com.threecubed.auber.GdxTestRunner;
+import com.threecubed.auber.Difficulty.Mode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(GdxTestRunner.class)
+public class DifficultyTest {
+
+  @Test
+  public void EasyTest() throws Exception {
+    Difficulty.load(Mode.EASY);
+    assertEquals(Mode.EASY, Difficulty.difficultyMode);
+    assertEquals("", 0.5f, Difficulty.damageMultiplier, 0f);    
+    assertEquals("", 0.5f, Difficulty.sabotageMultiplier, 0f);
+    assertEquals("", 0.5f, Difficulty.speedMultiplier, 0f);
+  }
+
+  @Test
+  public void MediumTest() throws Exception {
+    Difficulty.load(Mode.MEDIUM);
+    assertEquals(Mode.MEDIUM, Difficulty.difficultyMode);
+    assertEquals("", 1f, Difficulty.damageMultiplier, 0f);    
+    assertEquals("", 1f, Difficulty.sabotageMultiplier, 0f);
+    assertEquals("", 1f, Difficulty.speedMultiplier, 0f);
+  }
+
+  @Test
+  public void HardTest() throws Exception {
+    Difficulty.load(Mode.HARD);
+    assertEquals(Mode.HARD, Difficulty.difficultyMode);
+    assertEquals("", 2f, Difficulty.damageMultiplier, 0f);    
+    assertEquals("", 2f, Difficulty.sabotageMultiplier, 0f);
+    assertEquals("", 2f, Difficulty.speedMultiplier, 0f);
+  }
+}

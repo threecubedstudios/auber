d4b8df0db3680ec6ad38d8a3595c8a8aa8064812
vincent.zhu <1146388480@qq.com>
2021-02-08 15:25:35 +0000

    few changes

--- core/src/main/com/threecubed/auber/screens/MenuScreen.java
+++ core/src/main/com/threecubed/auber/screens/MenuScreen.java
@@ -3,6 +3,7 @@ package com.threecubed.auber.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Graphics.DisplayMode;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -82,8 +83,11 @@ public class MenuScreen extends ScreenAdapter {
     Runnable onContinueClick = new Runnable() {
       @Override
       public void run() {
-        continueGame = true;
+
+        Preferences pre = Gdx.app.getPreferences("aubergame");
+        continueGame = pre.getString("markForSaving", null) != null;
         game.setScreen(new GameScreen(game, false, difficulty));
+
       }
     };
 
